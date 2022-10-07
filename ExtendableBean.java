57f1deccf76829265ea3ab23c6129275e9314182
Angelo Sales <angelogustavo157@gmail.com>
2022-10-07 16:18:06 -0300

    Delete ExtendableBean.java

--- ExtendableBean.java
+++
@@ -1,9 +0,0 @@
-public class ExtendableBean {
-    public String name;
-    private Map<String, String> properties;
-
-    @JsonAnyGetter
-    public Map<String, String> getProperties() {
-        return properties;
-    }
-}
